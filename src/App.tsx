b431db0f8b62fc71e067b8e589570a90c6a07fe9
fcamblor2 <fcamblor@gmail.com>
2024-10-03 15:14:06 +0200

    step1: introduced Counter component + using 2 separate counter instances

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import { useState } from 'react'
+import {useState} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+let appRenderingCount = 0;
 
+function App() {
+  appRenderingCount++
+  console.log(`rendering App #${appRenderingCount}...`)
   return (
     <>
       <div>
@@ -18,9 +20,8 @@ function App() {
       </div>
       <h1>Vite + React ({process.env.NODE_ENV})</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <Counter initialValue={0} />
+        <Counter initialValue={42} />
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
@@ -29,6 +30,17 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
+  )
+}
+
+function Counter({ initialValue}: {initialValue: number}) {
+  console.log(`rendering Counter (from app rendering count #${appRenderingCount})...`)
+  const [count, setCount] = useState(initialValue)
+
+  return (
+    <button onClick={() => setCount((count) => count + 1)}>
+      count is {count}
+    </button>
   )
 }
 
